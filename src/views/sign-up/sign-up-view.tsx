f23ea5e974e012f456ad46e214a4077a6993fa78
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2022-09-05 18:37:40 -0400

    feat: :sparkles: adding form fields

--- src/views/sign-up/sign-up-view.tsx
+++ src/views/sign-up/sign-up-view.tsx
@@ -5,7 +5,7 @@ import Menu from '../shared/components/menu';
 import { H1 } from '../layout/layout.styles';
 import { getLocaleDateString } from '../shared/utils/locale-datetime';
 
-import { HookData } from './use-sign-up';
+import useSignUp, { HookData } from './use-sign-up';
 import {
   Wrapper,
   Form,
@@ -21,22 +21,78 @@ import {
 } from './sign-up.styles';
 
 const SignUpView: React.FC<HookData> = (): ReactElement => {
+  const {
+    data,
+    movies,
+    sitRows,
+    sitPlaces,
+    handleChangeData,
+    handleChangeAvatar,
+    handleSubmit,
+    handleClearAvatar,
+    handleChangeMovieId,
+    handleChangeSitRow,
+    handleChangeSitPlace,
+  } = useSignUp();
+
+
   return (
     <Wrapper>
       <Menu />
-
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Row center="xs">
           <Col xs={12} md={4}>
             <H1>Sign up for a movie</H1>
-
-            {/* FILL IN THE GAPS */}
-            {/* Fields go here ... */}
-
+            <Droparea >
+              <span>Thumbnail</span>
+              {data.avatarBase64 && (<>
+                <Preview>
+                  <Img src={data.avatarBase64} alt="Thumbnail" />
+                </Preview>
+                <ClearButton onClick={handleClearAvatar}>Delete Avatar</ClearButton></>)
+              }
+              {!data.avatarBase64 && <input type="file" name="file" onChange={handleChangeAvatar} />}
+            </Droparea>
+            <Label>
+              First Name:
+              <Input
+                type="text"
+                value={data.firstName}
+                onChange={(e) => handleChangeData("firstName", e.target.value)}
+              />
+            </Label><Label>
+              Last Name:
+              <Input type="text" value={data.lastName} onChange={(e) => handleChangeData("lastName", e.target.value)} />
+            </Label><Label>
+              Email:
+              <Input type="text" value={data.email} onChange={(e) => handleChangeData("email", e.target.value)} />
+            </Label><Label>
+              Phone:
+              <Input type="text" value={data.phone} onChange={(e) => handleChangeData("phone", e.target.value)} />
+            </Label><Label>
+              Movie:
+              {movies && (<Select onChange={(e) => handleChangeMovieId(e.target.value)} >
+                <option>--</option>
+                {movies.map((movie) => (<option key={movie.id} value={movie.id}>{movie.title} - {getLocaleDateString(movie.startDate)}</option>))}
+              </Select>)}
+            </Label><Label>
+              Sit Row:
+              <Select onChange={(e) => handleChangeSitRow(e.target.value)} >
+                <option>--</option>
+                {sitRows && sitRows.map(sit => (<option key={sit} value={sit}>{sit}</option>))}
+              </Select>
+            </Label><Label>
+              Sit Place:
+              <Select onChange={(e) => handleChangeSitPlace(e.target.value)} >
+                <option>--</option>
+                {sitPlaces && sitPlaces.map(sit => (<option key={sit} value={sit}>{sit}</option>))}
+              </Select>
+            </Label>
             <Button type="submit">Save</Button>
           </Col>
         </Row>
       </Form>
+
     </Wrapper>
   );
 };
